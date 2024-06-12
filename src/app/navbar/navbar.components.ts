fd6cff3bf08b67965ed0aa1992e0259e7af02395
ridoy.hossain <ridoy.hossain@bracits.com>
2024-06-12 12:51:17 +0600

    add different file add link to the templateUrl and StyleUrl

--- src/app/navbar/navbar.components.ts
+++ src/app/navbar/navbar.components.ts
@@ -3,12 +3,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector:'app-navbar',
-  template:`<h1>Navbar Template</h1>
-            <p>This is paragraph</p>`,
-  styles:[`h1{
-    color: red;
-    font-size: 50px;
-  }`]
+  templateUrl:'./navbar.components.html',
+  styleUrls:['./navbar.components.css']
 })
 export class NavbarComponents{
 
