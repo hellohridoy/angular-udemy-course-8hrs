5460b4fe5d0fa6e2f57d7a658ed761869bd06060
ridoy.hossain <ridoy.hossain@bracits.com>
2024-06-12 13:41:06 +0600

    data binding in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {NavbarComponents} from "./navbar/navbar.components";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponents
+    NavbarComponents,
+    PostComponent
   ],
   imports: [
     BrowserModule,
